f1a19eb2fff6a98f8656c66f8a69b01982dbe8b1
Jasim378 <145831981+Jasim378@users.noreply.github.com>
2024-01-08 16:43:24 +0000

    Add files via upload
    two number aad sum in array

--- first.js
+++ first.js
@@ -1,1 +1,9 @@
-
+let arr=[10,3,4,5,7,8,9];
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]%2!==0&&arr[j]%2!==0){
+            let sum=arr[i]+arr[j];
+            console.log("two number add is:",arr[i],"+",arr[j],"=",sum);
+        }
+    }
+}
