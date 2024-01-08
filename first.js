3365f6467afb6df47ce99e14ff6c3dc636f9de5a
Jasim378 <145831981+Jasim378@users.noreply.github.com>
2024-01-08 16:40:56 +0000

    Update first.js

--- first.js
+++ first.js
@@ -1,13 +1,1 @@
-let marks=[85,97,44,37,76,60];
-let sum=0;
-for(let mark of marks){
-    sum=sum+mark;
-    console.log(`avg marks of student ${mark/marks.length}`);
-    console.log(`marks of index is ${[mark]}`);
-}
-let avg=sum/marks.length;
-console.log("average marks is "+avg);
 
-for(let i=0;i<marks.length;i++){
-    console.log(`marks of index is = ${i}`);
-}
